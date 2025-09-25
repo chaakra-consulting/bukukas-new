91e3a54bec93afa74a99c0d01e0e330870f3e1f2
ikiraihan <ikiraihann@gmail.com>
2025-09-25 10:07:54 +0700

    fixing generate customer code

--- application/modules/master/controllers/Customers.php
+++ application/modules/master/controllers/Customers.php
@@ -104,9 +104,10 @@ class Customers extends MY_Controller
 
         ));
 
-        $options = in_array($this->input->post('jenis'),['SWASTA','BUMN','BUMD']) ? array("bentuk" => $this->input->post('bentuk')) : array("jenis" => $this->input->post('jenis')) ;
+        $options = in_array($this->input->post('jenis'),['SWASTA']) ? array("bentuk" => $this->input->post('bentuk'),"code_is_null" => true) : array("jenis" => $this->input->post('jenis'),"code_is_null" => true) ;
         $customers = $this->Master_Customers_model->get_details($options)->result();
         $jenisCode = get_code_jenis_customers($this->input->post('jenis'),$this->input->post('bentuk'));
+
         $count = str_pad(count($customers) + 1, 2, '0', STR_PAD_LEFT);
         $code = $jenisCode.'.'.$count;
 
@@ -148,8 +149,8 @@ class Customers extends MY_Controller
         $customer = $this->Master_Customers_model->get_details(array("id" => $customers_id))->row();
 
         if(!$customer->code){
-            // $options = in_array($this->input->post('jenis'),['SWASTA']) ? array("bentuk" => $this->input->post('bentuk'),"code_is_null" => true) : array("jenis" => $this->input->post('jenis'),"code_is_null" => true) ;
-            $options = array("jenis" => $this->input->post('jenis'),"code_is_null" => true) ;
+            $options = in_array($this->input->post('jenis'),['SWASTA']) ? array("bentuk" => $this->input->post('bentuk'),"code_is_null" => true) : array("jenis" => $this->input->post('jenis'),"code_is_null" => true) ;
+            //$options = array("jenis" => $this->input->post('jenis'),"code_is_null" => true) ;
             $customers = $this->Master_Customers_model->get_details($options)->result();
             $jenisCode = get_code_jenis_customers($this->input->post('jenis'),$this->input->post('bentuk'));
             $count = str_pad(count($customers) + 1, 2, '0', STR_PAD_LEFT);
