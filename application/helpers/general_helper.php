734894aaf23c00b9fdf84319926dbbab90d09034
ikiraihan <ikiraihann@gmail.com>
2025-09-25 13:26:56 +0700

    fix

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -1956,11 +1956,11 @@ if(!function_exists("terbilang")){
       elseif ($x < 100)
         return terbilang($x / 10) . " puluh" . terbilang($x % 10);
       elseif ($x < 200)
-        return "seratus" . terbilang($x - 100);
+        return " seratus" . terbilang($x - 100);
       elseif ($x < 1000)
         return terbilang($x / 100) . " ratus" . terbilang($x % 100);
       elseif ($x < 2000)
-        return "seribu" . terbilang($x - 1000);
+        return " seribu" . terbilang($x - 1000);
       elseif ($x < 1000000)
         return terbilang($x / 1000) . " ribu" . terbilang($x % 1000);
       elseif ($x < 1000000000)
