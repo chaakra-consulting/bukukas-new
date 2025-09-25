c7786eeeed696f99b2e15ebdd08eb17f1e5f933e
ikiraihan <ikiraihann@gmail.com>
2025-09-25 15:34:06 +0700

    refactor view

--- application/modules/sales/views/invoice/inv_status_bar.php
+++ application/modules/sales/views/invoice/inv_status_bar.php
@@ -1,39 +1,59 @@
-<div class="panel panel-default  p10 no-border m0">
-    <span class="ml15">Instansi : <?php 
-        echo (modal_anchor(get_uri("master/customers/view/" . $client_info->id),$client_info->name, array("data-post-id" => $client_info->id,"title" => "Vendors Info")));
-        ?>
-    </span> 
-    <span><?php echo $invoice_status_label; ?></span>  
+<style>
+    .info-row {
+        display: flex;
+        padding: 6px 10px;
+        background: #fff;
+    }
+    .info-label {
+        width: 180px;
+        font-weight: 600;
+        margin: 5px;
+    }
+    .info-colon {
+        width: 20px;
+        font-weight: 600;
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+    .info-value {
+        flex: 1;
+        margin-top: 5px;
+    }
+</style>
+
+<div class="info-row">
+    <div class="info-label">Instansi</div>
+    <div class="info-colon">:</div>
+    <div class="info-value">
+        <?php echo modal_anchor(
+            get_uri("master/customers/view/" . $client_info->id),
+            $client_info->name,
+            ["data-post-id" => $client_info->id,"title" => "Vendors Info"]
+        ); ?>
+        <?php echo $invoice_status_label; ?>
+    </div>
 </div>
-<div class="panel panel-default  p10 no-border m0">
-    <span></span>
-    <span class="ml15">Nama Projek : <?php 
-        //print_r($client_info);exit;
-        echo $item_info->title;
-        ?>
-    </span>   
+
+<div class="info-row">
+    <div class="info-label">Nama Projek</div>
+    <div class="info-colon">:</div>
+    <div class="info-value"><?php echo $item_info->title; ?></div>
 </div>
-<div class="panel panel-default  p10 no-border m0">
-    <span></span>
-    <span class="ml15">Tanggal SPK : <?php 
-        //print_r($client_info);exit;
-        echo $invoice_info->inv_date;
-        ?>
-    </span>   
+
+<div class="info-row">
+    <div class="info-label">Tanggal SPK</div>
+    <div class="info-colon">:</div>
+    <div class="info-value"><?php echo $invoice_info->inv_date; ?></div>
 </div>
-<div class="panel panel-default  p10 no-border m0">
-    <span></span>
-    <span class="ml15">Tanggal Kontrak : <?php 
-        //print_r($client_info);exit;
-        echo $invoice_info->inv_contract_date;
-        ?>
-    </span>   
+
+<div class="info-row">
+    <div class="info-label">Tanggal Kontrak Berakhir</div>
+    <div class="info-colon">:</div>
+    <div class="info-value"><?php echo $invoice_info->inv_contract_date; ?></div>
 </div>
-<div class="panel panel-default  p10 no-border m0">
-    <span></span>
-    <span class="ml15">Jumlah Termin : <?php 
-        //print_r($client_info);exit;
-        echo $invoice_info->termin;
-        ?>
-    </span>   
+
+<div class="info-row">
+    <div class="info-label">Jumlah Termin</div>
+    <div class="info-colon">:</div>
+    <div class="info-value"><?php echo $invoice_info->termin; ?></div>
 </div>
